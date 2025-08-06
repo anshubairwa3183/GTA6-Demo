a1c691b40826d0304517c9ccd2a64a9905ce212e
Anshu Bairwa <bairwaanshu3183@gmail.com>
2025-08-06 16:55:03 +0530

    Added the "mouse" text

--- src/App.jsx
+++ src/App.jsx
@@ -125,7 +125,7 @@ function App() {
       </div>
 
       {showContent && (
-        <div className='main w-full -rotate-20 scale-[1.7]'>
+        <div className="main w-full -rotate-20 scale-[1.7]">
           <div className="landing w-full h-screen bg-black">
             <div className="navbar absolute top-0 left-0 w-full py-10 px-10 z-10">
               <div className="logo flex gap-10">
@@ -134,33 +134,55 @@ function App() {
                   <div className="line w-8 h-1 bg-white"></div>
                   <div className="line w-6 h-1 bg-white"></div>
                 </div>
-                <h3 className='text-5xl text-white leading-none -mt-[12px]'>Rockstar</h3>
+                <h3 className="text-5xl text-white leading-none -mt-[12px]">
+                  Rockstar
+                </h3>
               </div>
             </div>
             <div className="imagediv relative overflow-hidden h-screen w-full">
-              <img src="/sky.png" alt="" className='sky scale-[1.5] -rotate-3 absolute top-0 left-0 w-full h-full object-cover'/>
-              <img src="/building.png" alt="" className='building scale-[1.8] -rotate-3 absolute top-0 left-0 w-full h-full object-cover'/>
+              <h3 className="absolute left-1/2 transform -translate-x-1/2 bottom-0 mb-10 text-5xl text-white z-50">
+                &larr; Move Your Mouse &rarr;
+              </h3>
+              <img
+                src="/sky.png"
+                alt=""
+                className="sky scale-[1.5] -rotate-3 absolute top-0 left-0 w-full h-full object-cover"
+              />
+              <img
+                src="/building.png"
+                alt=""
+                className="building scale-[1.8] -rotate-3 absolute top-0 left-0 w-full h-full object-cover"
+              />
               <div className="text absolute scale-[1.4] -rotate-10 flex flex-col gap-3 leading-none text-white top-0 left-1/2 -translate-x-1/2">
-              <h1 className='text-[12rem] leading-none -ml-20'>grand</h1>
-              <h1 className='text-[12rem] leading-none ml-40'>theft</h1>
-              <h1 className='text-[12rem] leading-none -ml-20'>auto</h1>
+                <h1 className="text-[12rem] leading-none -ml-20">grand</h1>
+                <h1 className="text-[12rem] leading-none ml-40">theft</h1>
+                <h1 className="text-[12rem] leading-none -ml-20">auto</h1>
+              </div>
+              <img
+                src="/character.png"
+                alt=""
+                className="absolute character scale-2 -rotate-10 bottom-[-170%] left-1/2 -translate-x-1/2 scale-[0.7] object-cover"
+              />
             </div>
-              <img src="/character.png" alt="" className='absolute character scale-2 -rotate-10 bottom-[-170%] left-1/2 -translate-x-1/2 scale-[0.7] object-cover'/>
-            </div>
+
             <div className="bottomnav text-white absolute bottom-0 left-0 w-full py-10 px-10 bg-gradient-to-t from-black to-transparent">
-              <div className="flex gap-4 items-center" >
+              <div className="flex gap-4 items-center">
                 <i className="text-3xl ri-arrow-down-line"></i>
-                <h3 className='text-2xl'>Scroll down</h3>
-                <img src="/xbox.webp" alt="" className='absolute h-[80px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'/>
+                <h3 className="text-2xl">Scroll down</h3>
+                <img
+                  src="/xbox.webp"
+                  alt=""
+                  className="absolute h-[80px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                />
               </div>
             </div>
           </div>
           <div className="w-full h-screen bg-black flex items-center justify-center">
-            <div className="inner flex w-full items-center justify-center h-[80%] gap-30" >
-              <img src="/girl.png" alt="" className='scale-[60%]'/>
+            <div className="inner flex w-full items-center justify-center h-[80%] gap-30">
+              <img src="/girl.png" alt="" className="scale-[60%]" />
               <div className="comming flex flex-col justify-center items-center -ml-[10%]">
-              <h1 className='text-white text-8xl'>Comming Soon</h1>
-              <h1 className='text-white text-7xl'>May 2027</h1>
+                <h1 className="text-white text-8xl">Comming Soon</h1>
+                <h1 className="text-white text-7xl">May 2027</h1>
               </div>
             </div>
           </div>
